87e0df45d9ba5b10bdd02892b4ee0a8eec90fb4c
Taras Ostapchuk <trs.ostp@gmail.com>
2024-02-24 20:46:38 +0100

    start project library, start app store

---
+++ store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from store.models import Book
+from store.serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
