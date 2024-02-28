59c17cc741856cb9f83168e696f2b0a20edddada
Taras Ostapchuk <trs.ostp@gmail.com>
2024-02-28 19:49:19 +0100

    Optimization query, add prefetch_related for readers

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from store.models import Book, UserBookRelation
+
+
+class BookReaderSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
 
 
 class BookSerializer(ModelSerializer):
@@ -9,11 +16,12 @@ class BookSerializer(ModelSerializer):
     rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
     sale_price = serializers.DecimalField(max_digits=7, decimal_places=2, read_only=True)
     owner_name = serializers.CharField(source='owner.username', default='', read_only=True)
+    readers = BookReaderSerializer(many=True, read_only=True)
 
     class Meta:
         model = Book
         fields = ('id', 'name', 'price', 'author_name', 'discount',
-                  'annotated_likes', 'rating', 'sale_price', 'owner_name')
+                  'annotated_likes', 'rating', 'sale_price', 'owner_name', 'readers')
 
 
 class UserBookRelationSerializer(ModelSerializer):
